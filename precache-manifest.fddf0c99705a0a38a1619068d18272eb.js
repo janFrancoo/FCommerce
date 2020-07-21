273d6542ea11124a4a374f44f64062727c1a0ab1
Eren Sırakaya <poisonweeb@gmail.com>
2020-07-21 20:20:13 +0300

    Updates

---
+++ precache-manifest.fddf0c99705a0a38a1619068d18272eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a21b7e7d969a7788a2442f423fe2bd",
+    "url": "/FCommerce/index.html"
+  },
+  {
+    "revision": "68b6b0787686e628f151",
+    "url": "/FCommerce/static/css/2.a49e1467.chunk.css"
+  },
+  {
+    "revision": "142e4bde90bf177b96c5",
+    "url": "/FCommerce/static/css/main.2e5eac12.chunk.css"
+  },
+  {
+    "revision": "68b6b0787686e628f151",
+    "url": "/FCommerce/static/js/2.10101f21.chunk.js"
+  },
+  {
+    "revision": "f2979e099ee4c06ef4b08e1c5db2b1ac",
+    "url": "/FCommerce/static/js/2.10101f21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142e4bde90bf177b96c5",
+    "url": "/FCommerce/static/js/main.cb2f1053.chunk.js"
+  },
+  {
+    "revision": "12a04e3586aa1bfa0464",
+    "url": "/FCommerce/static/js/runtime-main.c7f9d381.js"
+  }
+]);
